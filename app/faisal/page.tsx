d31be65509ff41cb65b8ff4b3a6f54f22030f4f9
faisalhrbk <faisalhrbk@outlook.com>
2025-05-19 21:11:53 +0500

    ahh shit here we go again! tutorial hell

---
+++ app/faisal/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is my first next js app</div>
+  )
+}
+
+export default page
---
+++ app/faisal/posts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is faisal's posts page</div>
+  )
+}
+
+export default page
